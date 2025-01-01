2e86fd74878a09214ab425e0282d3cdd7930b2a2
JakubDomon <jakubdomonn@gmail.com>
2025-01-01 18:58:20 +0100

    DTO Validation

--- API/DTO/Membership/RegisterUserDto.cs
+++ API/DTO/Membership/RegisterUserDto.cs
@@ -1,7 +1,27 @@
-﻿namespace API.DTO.Membership
+﻿using API.DTO.MessagesResources;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTO.Membership
 {
     public class RegisterUserDto
     {
-        public required string UserName { get; set; }
+        [Required(ErrorMessageResourceName = "UserLoginRequired", ErrorMessageResourceType = typeof(MembershipValidationMessages))]
+        [MinLength(5, ErrorMessageResourceName = "UserLoginTooShort", ErrorMessageResourceType = typeof(MembershipValidationMessages))]
+        public string? Login { get; set; }
+
+        [Required(ErrorMessageResourceName = "UserPasswordRequired", ErrorMessageResourceType = typeof(MembershipValidationMessages))]
+        [MinLength(8, ErrorMessageResourceName = "UserPasswordTooShort", ErrorMessageResourceType = typeof(MembershipValidationMessages))]
+        [RegularExpression(@"^(?=.*\\d.*\\d)(?=.*[!@#$%^&*(),.?\"":{}|<>]).*$", ErrorMessageResourceName = "UserPasswordConditionsNotMet", ErrorMessageResourceType = typeof(MembershipValidationMessages))]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessageResourceName = "UserEmailRequired", ErrorMessageResourceType = typeof(MembershipValidationMessages))]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessageResourceName = "UserEmailConditionsNotMet", ErrorMessageResourceType = typeof(MembershipValidationMessages))]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessageResourceName = "UserFirstNameRequired", ErrorMessageResourceType = typeof(MembershipValidationMessages))]
+        [MinLength(2, ErrorMessageResourceName = "UserFirstNameTooShort", ErrorMessageResourceType = typeof(MembershipValidationMessages))]
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
     }
 }
---
+++ DataAccess.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DataAccess.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
